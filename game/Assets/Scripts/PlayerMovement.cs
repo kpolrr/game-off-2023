c0b48a86ada319d751b4762e9c60b3d13c50b1ed
kpolr <106914480+kpolrr@users.noreply.github.com>
2023-11-11 23:59:55 +1100

    movement edits
    added drag and stable movement

--- game/Assets/Scripts/PlayerMovement.cs
+++ game/Assets/Scripts/PlayerMovement.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     public float movementSpeed;
 
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+
+    [Header("Ground")]
+    public float playerHeight = 2f;
+    public LayerMask ground;
+    bool grounded;
+
+    [Header("GameObjects")]
     public Transform orientation;
 
     float horizontalInput;
@@ -22,7 +34,16 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, ground);
+
         PlayerInput();
+        SpeedControl();
+
+        if (grounded) { rb.drag = groundDrag; } else { rb.drag = 0; }
+    }
+
+    void FixedUpdate()
+    {
         MovePlayer();
     }
 
@@ -37,5 +58,16 @@ public class PlayerMovement : MonoBehaviour
         moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
 
         rb.AddForce(moveDirection.normalized * movementSpeed, ForceMode.Force);
+    }
+
+    void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > movementSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * movementSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
     }
 }
